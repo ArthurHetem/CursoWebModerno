410574e7bae72a0018797bc777767263e34e0016
Arthur Hetem <arthurhetemgames@gmail.com>
2020-06-07 03:20:54 -0300

    Update With new Classes
    Aula 31 -  Aula 40
    
    Correção no Readme

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // acrescenta a ao b (b = b + a)
+console.log(b)
+
+b -= 4 // subtrai 4 de b (b = b - 4)
+console.log(b)
+
+b *= 2 // multiplica b por 2 (b = b * 2)
+console.log(b)
+
+b /= 2 // divide b por 2 (b = b / 2)
+console.log(b)
+
+b %= 2 // módulo de b (b = b & 2)
+console.log(b)
---
+++ fundamentos/hoisting.js
@@ -0,0 +1,7 @@
+console.log('a =', 2)
+var a = 2
+console.log('a =', 2)
+
+console.log('b =', b)
+let b = 5
+console.log('b =', b)
