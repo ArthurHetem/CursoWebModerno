3cce9abe2f10b63361244d59364db1fc5874e584
Arthur Hetem <arthurhetemgames@gmail.com>
2020-06-07 02:17:10 -0300

    More Classes and Readme
    Aula 24 - Aula 30
    Readme

---
+++ fundamentos/usandovar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            { 
+                var sera =  'Será???' 
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+}
+
+teste()
+console.log(local) // Não definida por causa do escopo da função
