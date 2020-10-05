67565c4490bd05a971325b0861be18226aa40e0a
Arthur Hetem <arthurhetemgames@gmail.com>
2020-10-04 22:46:33 -0300

    More classes
    Aulas até 310

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,35 @@
+const { parallel } = require('gulp')
+const gulp = require('gulp')
+const series = gulp.series
+
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!')
+    return cb()
+}
+
+function copiar(cb) {
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+        // .pipe(imagePelaMetade())
+        // .pipe(imageEmPretoEBranco())
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // .pipe(transformacaoC())
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
